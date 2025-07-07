fa9362fa853570c5c31c0467fccf777082ed275a
Neha Sharma <neha.iitjdh@gmail.com>
2025-07-08 00:55:04 +0530

    Basic Solution

---
+++ 9_Palindrome/1.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        temp = x
+        reverse=0
+        while (temp):
+            dig = temp%10
+            reverse = reverse*10 + dig
+            temp = temp/10
+        if (x==reverse):
+            return True
+        else:
+            return False
+        
+# Getting "Time Limit Exceeded" on LeetCode
