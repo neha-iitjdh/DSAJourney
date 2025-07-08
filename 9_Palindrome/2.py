650593598fa8c4541aea7362200834a0499a4e97
Neha Sharma <neha.iitjdh@gmail.com>
2025-07-08 23:10:11 +0530

    Debug Comments Added

--- 9_Palindrome/2.py
+++ 9_Palindrome/2.py
@@ -1,11 +1,13 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        if x<0 or (x !=0 and x%10 ==0):
-            return False
-        half = 0
-        while x >half:
-            half =(half * 10) + (x%10)
-            x=x//10
-        return x == half or x ==half //10
+def isPalindrome(x):
+    if x<0 or (x !=0 and x%10 ==0):
+        return False
+    half = 0
+    while x >half:
+        half =(half * 10) + (x%10)
+        x=x//10
+        print(f"half: {half}, x: {x}")
+    return x == half or x ==half //10
 
 # This works only on half of the input
+
+print(isPalindrome(1213443121))  # True
