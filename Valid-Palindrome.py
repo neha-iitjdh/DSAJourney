fe54b4c0b79ef489b615cb821b406e225972381f
Neha Sharma <neha.iitjdh@gmail.com>
2025-09-28 17:18:20 +0530

    [125] [Time Beats: 98.94%] [Memory Beats: 27.63%] - LeetPush

--- Valid-Palindrome.py
+++ Valid-Palindrome.py
@@ -1,18 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         #APPROACH 1 but time exceeding
-        fp=0
-        lp=len(s)-1
-        while fp<=lp:
-            if(s[fp].isalnum())==False:
-                fp+=1
-                continue
-            if(s[lp].isalnum())==False:
-                lp-=1
-                continue
-            if(s[fp].lower()!=s[lp].lower()):
-                return False
-            fp+=1
-            lp-=1
-        return True
-        
+        # fp=0
+        # lp=len(s)-1
+        # while fp<=lp:
+        #     if(s[fp].isalnum())==False:
+        #         fp+=1
+        #         continue
+        #     if(s[lp].isalnum())==False:
+        #         lp-=1
+        #         continue
+        #     if(s[fp].lower()!=s[lp].lower()):
+        #         return False
+        #     fp+=1
+        #     lp-=1
+        # return True
+        ss = s.strip().lower()
+        l = []
+        for i in ss:
+            if i.isalnum():
+                l.append(i)
+        return l == l[::-1]
