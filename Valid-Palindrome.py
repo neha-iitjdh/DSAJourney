9b6328fb2d5cc4f9db4f986a2544f67e05505399
Neha Sharma <neha.iitjdh@gmail.com>
2025-09-28 17:17:00 +0530

    [125] [Time Beats: 34.72%] [Memory Beats: 95.23%] - LeetPush

---
+++ Valid-Palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #APPROACH 1 but time exceeding
+        fp=0
+        lp=len(s)-1
+        while fp<=lp:
+            if(s[fp].isalnum())==False:
+                fp+=1
+                continue
+            if(s[lp].isalnum())==False:
+                lp-=1
+                continue
+            if(s[fp].lower()!=s[lp].lower()):
+                return False
+            fp+=1
+            lp-=1
+        return True
+        
