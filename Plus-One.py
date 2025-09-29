208f8f6a633524ffe7f62f7617026522aa3d026a
Neha Sharma <neha.iitjdh@gmail.com>
2025-09-29 17:41:42 +0530

    [66] [Time Beats: 100.00%] [Memory Beats: 8.63%] - LeetPush

--- Plus-One.py
+++ Plus-One.py
@@ -1,9 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        numeric = 0
-        for i in digits :
-            numeric = numeric*10+i
-        new_numeric = numeric+1
-        return [int(i) for i in str(new_numeric)]
+        # Approach1
+        # numeric = 0
+        # for i in digits :
+        #     numeric = numeric*10+i
+        # new_numeric = numeric+1
+        # return [int(i) for i in str(new_numeric)]
+        x=int(''.join([str(i) for i in digits]))
+        y=[int(j) for j in str(x+1)]
+        return y
 
         
