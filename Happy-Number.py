3ae44bfa366388e7d597b3efc0ee690ffbe91ea6
Neha Sharma <neha.iitjdh@gmail.com>
2025-09-30 21:18:29 +0530

    [202] [Time Beats: 100.00%] [Memory Beats: 18.16%] - LeetPush

--- Happy-Number.py
+++ Happy-Number.py
@@ -1,15 +1,24 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()        
-        while n != 1 and n not in seen:
+        #Approach1
+        # seen = set()        
+        # while n != 1 and n not in seen:
+        #     seen.add(n)
+        #     sumo = 0
+        #     while n > 0:
+        #         dig = n % 10
+        #         n //= 10
+        #         sumo += dig ** 2
+        #     n = sumo        
+        # return n == 1
+        seen = set()
+        while True:
+            if n in seen:
+                return False
             seen.add(n)
-            sumo = 0
-            while n > 0:
-                dig = n % 10
-                n //= 10
-                sumo += dig ** 2
-            n = sumo        
-        return n == 1
+            if n == 1:
+                return True
+            n = sum([int(x)**2 for x in list(str(n))])
         
         
 
