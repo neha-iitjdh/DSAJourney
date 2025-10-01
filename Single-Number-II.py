c1b53aeeb760cfc5d4f10cd0d634d81a670bb725
Neha Sharma <neha.iitjdh@gmail.com>
2025-10-01 19:08:15 +0530

    [137] [Time Beats: 44.05%] [Memory Beats: 7.72%] - LeetPush

--- Single-Number-II.py
+++ Single-Number-II.py
@@ -1,3 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return int((3*sum(set(nums))-sum(nums))/2)
+        #Approach1
+        #return int((3*sum(set(nums))-sum(nums))/2)
+        #Approach2
+        counts = Counter(nums)
+        once = [item for item, count in counts.items() if count == 1]
+        return once[0]
