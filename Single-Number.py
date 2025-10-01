616cf6e5155ad255c121e614106e7ed66cea0a9d
Neha Sharma <neha.iitjdh@gmail.com>
2025-10-01 19:02:55 +0530

    [136] [Time Beats: 64.29%] [Memory Beats: 21.38%] - LeetPush

--- Single-Number.py
+++ Single-Number.py
@@ -1,4 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return (2*sum(set(nums))-sum(nums))
+        #approach 1
+        #return (2*sum(set(nums))-sum(nums))
+        #approach2
+        number = 0
+        for num in nums:
+            number = number ^ num
+        return number
         
