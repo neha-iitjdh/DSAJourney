af84ceae83f7ce68e98e7f257afd8df942ae4e40
Neha Sharma <neha.iitjdh@gmail.com>
2025-10-01 19:32:43 +0530

    [169] [Time Beats: 100.00%] [Memory Beats: 50.45%] - LeetPush

--- Majority-Element.py
+++ Majority-Element.py
@@ -6,6 +6,26 @@ class Solution:
         # for i in count_list.keys():
         #     if count_list[i]==max_count:
         #         return i
-        nums_dict =Counter(nums)
-        return max(nums_dict , key = nums_dict.get)
+        #Approach2
+        # nums_dict =Counter(nums)
+        # return max(nums_dict , key = nums_dict.get)
+        #Approach3
+        # element=nums[0]
+        # c=1
+        # n=len(nums)
+        # for i in range(1,n):
+        #     if nums[i]==element:
+        #         c+=1
+        #     else:
+        #         c-=1
+        #         if c==0:
+        #             element=nums[i]
+        #             c=1
+        # return element
+        #Approach 4
+        # nums.sort()
+        # n = len(nums)
+        # return nums[n//2]
+        #Approach5
+        return max(set(nums), key=nums.count)
         
