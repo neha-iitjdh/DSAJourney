e0a583a9478d35dab08d565ca70a4f57c719fac0
Neha Sharma <neha.iitjdh@gmail.com>
2025-10-01 18:47:58 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 51.54%] - LeetPush

--- Missing-Number.py
+++ Missing-Number.py
@@ -1,8 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        miss=0
-        for i in range(len(nums)+1):
-            if i in nums:
-                pass
-            else:
-                return i
+        #approach 1
+        # miss=0
+        # for i in range(len(nums)+1):
+        #     if i in nums:
+        #         pass
+        #     else:
+        #         return i
+        n = len(nums)
+        return int((n*(n+1)/2)-(sum(nums)))
