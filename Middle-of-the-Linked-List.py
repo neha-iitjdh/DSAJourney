ef369457a9e5302126f5f3c2ee0f0a68a6809a2e
Neha Sharma <neha.iitjdh@gmail.com>
2026-01-21 02:03:07 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 6.12%] - LeetPush

--- Middle-of-the-Linked-List.py
+++ Middle-of-the-Linked-List.py
@@ -5,16 +5,24 @@
 5#         self.next = next
 6class Solution:
 7    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-8        length = 0
-9        current = head
-10        while current:
-11            length += 1
-12            current = current.next
-13        
-14        middle_pos = length // 2          
-15        current = head
-16        for i in range(middle_pos):
-17            current = current.next
-18        
-19        return current
-20        
+8        #brute force  approach
+9        # length = 0
+10        # current = head
+11        # while current:
+12        #     length += 1
+13        #     current = current.next
+14        
+15        # middle_pos = length // 2          
+16        # current = head
+17        # for i in range(middle_pos):
+18        #     current = current.next
+19        
+20        # return current
+21        # optimized
+22        slow = fast = head
+23        
+24        while fast and fast.next:
+25            slow = slow.next      
+26            fast = fast.next.next 
+27        return slow
+28        
